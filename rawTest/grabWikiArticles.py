97917e8409802777cb603565b79fc0214a99fe4a
miphung <phung1@stolaf.edu>
2019-06-14 16:31:48 -0500

    adding last doc

--- rawTest/grabWikiArticles.py
+++ rawTest/grabWikiArticles.py
@@ -1,10 +1,30 @@
 #using Wikipedia to pull out the main articles
 
 import wikipedia as wk
-
+'''
+#the first wiki article 
 art1 = wk.page("Indian rhinoceros")
 ih = open("wikiIndianRhino.txt","w", encoding="utf-8")
 a1 = art1.content
-#for i in range(len(a1)):
 ih.write(a1)
 ih.close()
+
+#second wiki article on black rhinos
+art2 = wk.page("Black rhinoceros")
+ih = open("wikiBlRhino.txt","w", encoding="utf-8")
+ih.write(art2.content)
+ih.close()
+
+#third wiki article on white rhinos
+art3 = wk.page("White rhinoceros")
+ih = open("wikiWhRhino.txt","w", encoding="utf-8")
+ih.write(art3.content)
+ih.close()
+'''
+#fourth wiki article on rhinos
+art4 = wk.page("Rhinoceros")
+ih = open("wikiRhino.txt","w", encoding="utf-8")
+ih.write(art4.content)
+ih.close()
+
+
