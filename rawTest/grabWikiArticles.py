cd8b4fd41198544966ef2396fec0370d72bf1a52
miphung <phung1@stolaf.edu>
2019-06-24 11:52:43 -0500

    updated files with sentence separation

--- rawTest/grabWikiArticles.py
+++ rawTest/grabWikiArticles.py
@@ -4,41 +4,42 @@
 
 from __future__ import unicode_literals, print_function
 import spacy
-spacy.load('en_core_web_sm')
-from spacy.lang.en import English
 import wikipedia as wk
 
-nlp = English()
+nlp = spacy.load("en_core_web_sm")
 
 #the first wiki article 
 art1 = wk.page("Indian rhinoceros")
 ih = open("wikiIndianRhino.txt","w", encoding="utf-8")
 a1 = nlp(art1.content)
 for s in a1.sents:
-	ih.write(asent)
+	ih.write(s.text)
+	ih.write("\n")
 ih.close()
-'''
+
 #second wiki article on black rhinos
 art2 = wk.page("Black rhinoceros")
 ih = open("wikiBlRhino.txt","w", encoding="utf-8")
 a2 = nlp(art2.content)
-bsent = [s.string.strip() for s in a2.sents]
-ih.write(bsent)
+for s in a2.sents:
+	ih.write(s.text)
+	ih.write("\n")
 ih.close()
 
 #third wiki article on white rhinos
 art3 = wk.page("White rhinoceros")
 ih = open("wikiWhRhino.txt","w", encoding="utf-8")
 a3 = nlp(art3.content)
-csent = [s.string.strip() for s in a3.sents]
-ih.write(csent)
+for s in a3.sents:
+	ih.write(s.text)
+	ih.write("\n")
 ih.close()
 
 #fourth wiki article on rhinos
 art4 = wk.page("Rhinoceros")
 ih = open("wikiRhino.txt","w", encoding="utf-8")
 a4 = nlp(art4.content)
-dsent = [s.string.strip() for s in a4.sents]
-ih.write(dsent)
+for s in a4.sents:
+	ih.write(s.text)
+	ih.write("\n")
 ih.close()
-'''
